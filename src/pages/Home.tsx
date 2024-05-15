ba39919d61b94b336397730aed5d5c14eafd437e
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 20:48:26 -0300

    Structure site with hybrid a navigation (pages and sections)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Profile from "./Profile";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Profile />
+      <About />
+      <Experience />
+      <Contact />
+    </>
+  );
 };
 
 export default Home;
