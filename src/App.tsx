0e9dd739e2e48a01cf3fd07cbd358f07ac686ca5
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 20:31:38 -0300

    Rename title.tsx to AppTitle.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { APP_TITLE } from "./constants";
-import AppTitle from "./components/app/title";
+import AppTitle from "./components/app/AppTitle";
 import Navbar from "./components/NavBar/Navbar";
 
 function App() {
