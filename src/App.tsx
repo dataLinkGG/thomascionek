0c09313c0f11d6d264ef9d36861dd3b44392dc64
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 20:01:21 -0300

    Add NavBar, but with non-funcional hambuger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { APP_TITLE } from "./constants";
 import AppTitle from "./components/app/title";
+import Navbar from "./components/NavBar/Navbar";
 
 function App() {
   return (
     <>
       <AppTitle title={APP_TITLE} />
+      <Navbar />
     </>
   );
 }
---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import styles from "./Navbar.module.css";
+
+const Nav = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1400);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 1400);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className={styles.navigation}>
+      {isMobile ? (
+        <nav id="hamburgerNav">
+          <div className={styles.logo}>Thomas Cionek</div>
+          <div className={styles.hamburgerMenu}>
+            <div className={styles.hamburgerIcon} onClick={toggleMenu}>
+              <span />
+              <span />
+              <span />
+            </div>
+            <ul className={`${styles.menuLinks} ${isOpen ? styles.open : ""}`}>
+              <li>
+                <a href="/" onClick={toggleMenu}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about" onClick={toggleMenu}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/experience" onClick={toggleMenu}>
+                  Experience
+                </a>
+              </li>
+              <li>
+                <a href="/contact" onClick={toggleMenu}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      ) : (
+        <nav>
+          <div className={styles.logo}>Thomas Cionek</div>
+          <div>
+            <ul className={styles.navLinks}>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/experience">Experience</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
+    </div>
+  );
+};
+
+export default Nav;
