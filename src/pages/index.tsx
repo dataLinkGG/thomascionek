96de0a62a0bdbb81dee2d8a9ab0a5ad10ff7d703
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 20:44:01 -0300

    Add profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,6 @@ import Home from "./Home.tsx";
 import About from "./About.tsx";
 import Contact from "./Contact.tsx";
 import Experience from "./Experience.tsx";
+import Profile from "./Profile.tsx";
 
-export { Home, About, Contact, Experience };
+export { Home, About, Contact, Experience, Profile };
