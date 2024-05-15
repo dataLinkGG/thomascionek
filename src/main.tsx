099d06a9e03601739cc051cf810e15d904178023
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 20:39:36 -0300

    Route pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, About, Contact, Experience } from "./pages";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/experience",
+        element: <Experience />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import Home from "./Home.tsx";
+import About from "./About.tsx";
+import Contact from "./Contact.tsx";
+import Experience from "./Experience.tsx";
+
+export { Home, About, Contact, Experience };
