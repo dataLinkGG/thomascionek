56059392aa664698583626e83b5af5059868bb5c
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 21:13:56 -0300

    Add more constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,3 +2,6 @@ export const NAME = "Thomas Cionek";
 export const ROLE = "Junior Full Stack Developer";
 export const LINKEDIN = "https://www.linkedin.com/in/thomas-cionek-123669145/";
 export const GITHUB = "https://github.com/dataLinkGG";
+export const EMAIL = "thomascionek97@gmail.com";
+export const WHATSAPP = "https://wa.me/5541988409682";
+export const PHONE = " +55 (41) 98840-9682";
