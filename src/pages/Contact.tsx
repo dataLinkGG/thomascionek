adce499369f3e406f744de42bdc8b0e4215c8f44
Thomas Cionek <thomascionek97@gmail.com>
2024-05-15 22:07:59 -0300

    Add contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Button, Tooltip } from "antd";
+import { WHATSAPP } from "../constants";
 
-const Contact: React.FC = () => {
-  return <div>Contact</div>;
+const Contact = () => {
+  return (
+    <section id="contact">
+      <p className="section__text__p1">Get in Touch</p>
+      <h1 className="title">Contact Me</h1>
+      <Tooltip placement="right" title={"Click or scan to chat with me!"}>
+        <Button
+          type="text"
+          target="_blank"
+          onClick={() => {
+            window.open(WHATSAPP, "_blank");
+          }}
+        >
+          <div>
+            <img
+              className="qr-code"
+              src="src/assets/qr_black_white_rounded.png"
+              alt=""
+            />
+          </div>
+        </Button>
+        <br /> {/* just put some element so the tooltip apears in the middle */}
+      </Tooltip>
+    </section>
+  );
 };
 
 export default Contact;
