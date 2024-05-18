0d2498cc8e92dfca31ad96a2ccc83adb167318b4
Thomas Cionek <thomascionek97@gmail.com>
2024-05-18 11:06:58 -0300

    Card button relative to card, on the bottom

--- src/components/AntModal/AntModal.tsx
+++ src/components/AntModal/AntModal.tsx
@@ -1,41 +1,18 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Modal } from "antd";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 
 type AntModalProps = {
   children: ReactNode;
+  isModalOpen: boolean;
+  handleOk: VoidFunction;
+  handleCancel: VoidFunction;
 };
 
-const AntModal: React.FC<AntModalProps> = ({ children }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
+const AntModal: React.FC<AntModalProps> = ({ children, isModalOpen }) => {
   return (
     <>
-      <Button
-        style={{
-          background: "var(--primary-color)",
-          color: "black",
-          border: "none",
-        }}
-        onClick={showModal}
-      >
-        <div>
-          <FontAwesomeIcon icon={faSearch} /> See More
-        </div>
-      </Button>
       <Modal
         title="Eduction"
         open={isModalOpen}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,14 +3,28 @@ import {
   faBriefcase,
   faBullseye,
   faGraduationCap,
+  faSearch,
 } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
-import { Tooltip } from "antd";
-import AntModal from "../components/AntModal/AntModal";
+import React, { useState } from "react";
 import { SendOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
 
 const About: React.FC = () => {
   const jobTitle = "Full Stack Development";
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <section id="about">
@@ -22,66 +36,104 @@ const About: React.FC = () => {
             <div className="details-container">
               <FontAwesomeIcon className="icon" icon={faBriefcase} />
               <h3>Experience</h3>
+              <br />
               <p>
                 {jobTitle}
                 <br />
                 Alude YC W20
               </p>
-              <div>
-                <AntModal>
-                  Worked with Python, JavaScript, TypeScript, and PostgreSQL.
-                  Interactively customized React components from Ant Design
-                  based on the product models of Figma, working in Scrum
-                  methodology, with Code Reviews, product and team
-                  retrospectives meetings. Engagingly implemented back-end
-                  library functionalities and actively interacted with the
-                  database.
-                </AntModal>
+              <div className="centeredButton">
+                <Button
+                  style={{
+                    background: "var(--primary-color)",
+                    color: "black",
+                    border: "none",
+                  }}
+                  onClick={showModal}
+                >
+                  <div>
+                    <FontAwesomeIcon icon={faSearch} /> See More
+                  </div>
+                </Button>
+              </div>
+              <div className="AntModal">
+                <Modal
+                  title="Eduction"
+                  open={isModalOpen}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                  okText="Ok"
+                  cancelText="Close"
+                  footer
+                >
+                  <div>
+                    Solid foundation in Linux, having used various distributions
+                    for nearly 10 years, with a keen interest in experimenting
+                    with commands, customization, and troubleshooting. Gained
+                    international exposure during a 4-year stint in Poland,
+                    where he studied Computer Science and interacted with
+                    exchange students from around the world.
+                  </div>
+                </Modal>
               </div>
             </div>
             <div className="details-container">
               <div>
                 <FontAwesomeIcon className="icon" icon={faGraduationCap} />
                 <h3>Education</h3>
+                <br />
                 <p>Strategic Planning and Analysis Systems Development</p>
+                <div className="centeredButton">
+                  <Button
+                    style={{
+                      background: "var(--primary-color)",
+                      color: "black",
+                      border: "none",
+                    }}
+                    onClick={showModal}
+                  >
+                    <div>
+                      <FontAwesomeIcon icon={faSearch} /> See More
+                    </div>
+                  </Button>
+                </div>
               </div>
-              <AntModal>
-                Solid foundation in Linux, having used various distributions for
-                nearly 10 years, with a keen interest in experimenting with
-                commands, customization, and troubleshooting. Gained
-                international exposure during a 4-year stint in Poland, where he
-                studied Computer Science and interacted with exchange students
-                from around the world.
-              </AntModal>
+              <Modal>
+                Worked with Python, JavaScript, TypeScript, and PostgreSQL.
+                Interactively customized React components from Ant Design based
+                on the product models of Figma, working in Scrum methodology,
+                with Code Reviews, product and team retrospectives meetings.
+                Engagingly implemented back-end library functionalities and
+                actively interacted with the database.
+              </Modal>
             </div>
-            <Tooltip>
-              <div className="details-container">
-                <div className="details-container-goals">
-                  <FontAwesomeIcon className="icon" icon={faBullseye} />
-                  <h3>Goals</h3>
-                  <div>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "flex-start",
-                      }}
-                    >
-                      <SendOutlined />
-                      &nbsp;Full Stack Development
-                    </div>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "flex-start",
-                      }}
-                    >
-                      <SendOutlined />
-                      &nbsp;Freelancing Projects
-                    </div>
+            <div className="details-container">
+              <div className="details-container-goals">
+                <FontAwesomeIcon className="icon" icon={faBullseye} />
+                <h3>Goals</h3>
+                <br />
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    <SendOutlined />
+                    &nbsp;Full Stack Development
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    <SendOutlined />
+                    &nbsp;Freelancing Projects
                   </div>
                 </div>
               </div>
-            </Tooltip>
+            </div>
           </div>
         </div>
       </div>
