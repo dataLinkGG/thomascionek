40b6c6fffc209a0dac5828112bf61b051eb2ff4d
Thomas Cionek <thomascionek97@gmail.com>
2024-05-18 22:20:12 -0300

    Create better organized style; card

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,37 @@
-import QRCode from "../components/QRCode/QRCode";
+import qr_code from "../assets/qr_black_white_rounded.png";
 
 const Contact: React.FC = () => {
   return (
     <section id="contact">
-      <p className="section__text__p1">Get in Touch</p>
-      <h1 className="title">Contact Me</h1>
-      <QRCode />
+      <p className="section__text__p1">Get in touch</p>
+      <h1 className="title">Contact me</h1>
+      <div className="section-container">
+        <div className="card">
+          <img className="qr-code" src={qr_code} alt="" />
+          <div className="vertical-divider"></div>
+          <div className="socials-card">
+            <div className="icon-wrapper">
+              <img
+                className="largeIcon"
+                src={
+                  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
+                }
+                alt="Social Icon 1"
+              />
+            </div>
+            <div className="icon-wrapper">
+              <img
+                className="largeIcon"
+                src={
+                  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+                }
+                alt="Social Icon 2"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
-
 export default Contact;
