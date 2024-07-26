c1831e1a16f25907536dcb52d5010972cbf1a8ae
Thomas Cionek <thomascionek97@gmail.com>
2024-07-26 16:12:13 -0300

    Add return type to HeroPic

--- src/components/HeroPic/HeroPic.tsx
+++ src/components/HeroPic/HeroPic.tsx
@@ -1,7 +1,7 @@
 import profilePic from "../../assets/profile-pic.jpg";
 import styles from "./HeroPic.module.css";
 
-const HeroPic = () => {
+const HeroPic = (): JSX.Element => {
   return (
     <img
       className={styles.profilePicture}
